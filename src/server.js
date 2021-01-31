c939b04deea59517d8cadc8b47e8d77690363a2b
Julian Poyourow <julianpoyo@gmail.com>
2021-01-31 10:02:06 -0800

    feat: Add p-queue to implement new PREDICTION_CONCURRENCY arg

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const os = require('os');
+const { default: PQueue } = require('p-queue');
+
 const predict = require('./predict.js');
 
 const app = express();
 app.use(morgan(':method :url :status :response-time ms'));
 app.use(cors());
 app.use(express.json());
+
+// Use PREDICTION_CONCURRENCY to limit CPU and memory usage when handling many parallel requests
+const PREDICTION_CONCURRENCY = parseInt(process.env.PREDICTION_CONCURRENCY || os.cpus().length, 10);
+
+const queue = new PQueue({
+  concurrency: PREDICTION_CONCURRENCY
+});
 
 app.post('/', async (req, res) => {
   const { sentences } = req.body;
@@ -15,9 +25,11 @@ app.post('/', async (req, res) => {
     return;
   }
 
-  const results = await predict(sentences);
+  queue.add(async () => {
+    const results = await predict(sentences);
 
-  res.status(200).json(results);
+    res.status(200).json(results);
+  });
 });
 
 const server = app.listen(parseInt(process.env.PORT || 3000, 10));
