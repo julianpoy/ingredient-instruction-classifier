7c0b63172758419f6d8299726fc7b04c26fb6d27
Julian Poyourow <julianpoyo@gmail.com>
2022-02-06 20:35:58 -0800

    feat: ARM Support

--- scripts/build_push_docker.sh
+++ scripts/build_push_docker.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-docker build . -t julianpoy/ingredient-instruction-classifier:latest -t julianpoy/ingredient-instruction-classifier:$1
+docker buildx build . \
+  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  -t julianpoy/ingredient-instruction-classifier:latest \
+  -t julianpoy/ingredient-instruction-classifier:$1
 
 docker push julianpoy/ingredient-instruction-classifier:$1
 docker push julianpoy/ingredient-instruction-classifier:latest
