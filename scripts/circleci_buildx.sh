c21b77f60be5952209a3fc0bba954afcaac89593
Julian Poyourow <julian@poyourow.com>
2022-02-07 05:12:00 +0000

    fix: Dirname

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -4,4 +4,6 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
 docker buildx create --use multi-arch-build
-sh ./build_push_docker.sh $1
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+sh $DIR/build_push_docker.sh $1
