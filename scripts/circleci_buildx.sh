b74c424f65da903261e88b8e445de93319f5e196
Julian Poyourow <julian@poyourow.com>
2022-02-07 05:15:19 +0000

    fix: Syntax for buildx use

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -3,7 +3,7 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
-docker buildx create --use multi-arch-build
+docker buildx use multi-arch-build
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 sh $DIR/build_push_docker.sh $1
