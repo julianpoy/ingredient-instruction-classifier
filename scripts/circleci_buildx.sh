79f848807ca8471fde2a84479f81a1ce9eda82d4
Julian Poyourow <julian@poyourow.com>
2022-02-07 05:22:14 +0000

    fix: Try installing buildx manually as docker plugin

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -2,6 +2,12 @@
 
 export DOCKER_BUILD_KIT=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
+
+wget https://github.com/docker/buildx/releases/download/v0.4.1/buildx-v0.4.1.linux-amd64
+chmod a+x buildx-v0.4.1.linux-amd64
+mkdir -p ~/.docker/cli-plugins
+mv buildx-v0.4.1.linux-amd64 ~/.docker/cli-plugins/docker-buildx
+
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
 docker buildx use multi-arch-build
