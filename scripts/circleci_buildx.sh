de6ef3de245c0e2560e999007c75d8be49839a85
Julian Poyourow <julianpoyo@gmail.com>
2022-02-06 21:06:58 -0800

    fix: Pass version arg through buildx proxy

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -2,4 +2,4 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
 docker buildx create --use multi-arch-build
-sh ./build_push_docker.sh
+sh ./build_push_docker.sh $1
