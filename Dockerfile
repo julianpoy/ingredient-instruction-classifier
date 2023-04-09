1fbe8964edfeeddaac908bb6c14b5be12ac60040
Julian Poyourow <julian@poyourow.com>
2023-04-09 20:54:16 +0000

    fix: buildx

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ENV MODEL_URL=file:///app/model/model.json
 COPY package.json package.json
 COPY package-lock.json package-lock.json
 
-RUN npm install
+RUN npm install --ci
 
 COPY ./src /app/src
 COPY ./model /app/model
--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -3,10 +3,12 @@
 export DOCKER_BUILD_KIT=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-wget https://github.com/docker/buildx/releases/download/v0.4.1/buildx-v0.4.1.linux-amd64
-chmod a+x buildx-v0.4.1.linux-amd64
+wget https://github.com/docker/buildx/releases/download/v0.10.4/buildx-v0.10.4.linux-amd64
+chmod a+x buildx-v0.10.4.linux-amd64
 mkdir -p ~/.docker/cli-plugins
-mv buildx-v0.4.1.linux-amd64 ~/.docker/cli-plugins/docker-buildx
+mv buildx-v0.10.4.linux-amd64 ~/.docker/cli-plugins/docker-buildx
+
+docker run --privileged --rm tonistiigi/binfmt --install all
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
