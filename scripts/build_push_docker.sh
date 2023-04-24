d663efc84206ee0f1fbcb9d1f65fc8eaaca6ee16
Julian Poyourow <julian@poyourow.com>
2023-04-24 23:07:05 +0000

    buildx

--- scripts/build_push_docker.sh
+++ scripts/build_push_docker.sh
@@ -3,11 +3,8 @@
 set -e
 
 docker buildx build . \
+  --push \
   --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
   -t julianpoy/ingredient-instruction-classifier:latest \
   -t julianpoy/ingredient-instruction-classifier:$1
 
-docker images --all
-
-#docker push julianpoy/ingredient-instruction-classifier:$1
-#docker push julianpoy/ingredient-instruction-classifier:latest
