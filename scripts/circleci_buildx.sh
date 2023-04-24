b870f24b3a2d9f55c8f6fb2b38da554f65cce2b3
Julian Poyourow <julian@poyourow.com>
2023-04-24 23:07:05 +0000

    buildx

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -17,4 +17,5 @@ docker context create multi-arch-build
 docker buildx create --use multi-arch-build
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-sh $DIR/build_push_docker.sh TESTING
+sh $DIR/build_push_docker.sh $1
+
